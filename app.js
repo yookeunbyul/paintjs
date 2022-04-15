22cdb9516077989a275b957282125e31ba084c6d
yookeunbyul <91243651+yookeunbyul@users.noreply.github.com>
2022-04-15 23:06:51 +0900

    Changing Color

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+//실제 픽셀 사이즈 설정
+canvas.width = 700;
+canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -26,10 +31,10 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
-
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -37,3 +42,5 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
